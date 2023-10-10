c9debaef0c091922578a17e603ee42f0ee1b85dc
subhammahanty235 <subhammahanty235@gmail.com>
2023-10-10 23:13:18 +0530

    API integation for name and picture done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,34 @@ import React from 'react'
 import '../styles/navbar.css'
 import DownArrowIcon from '../assets/icons/down-arrow-svgrepo-com.svg'
 import SearchIcon from '../assets/icons/search-svgrepo-com.svg'
+import { useSelector } from 'react-redux'
 const Navbar = () => {
-  return (
-    <nav>
-        <div className="logo">LoGo</div>
-        <div className="searchbar">
-            <img src={SearchIcon} alt="" className='searchIcon'/>
-            <input type="text" />
-        </div>
-        <div className="menus">
-            <button className="loginbtn">Login</button>
-            <div className="loggedInMenus">
-                <img src="https://lh3.googleusercontent.com/a/ACg8ocJhQGbw3iuf5B0LgW4ydvNzdsG0XehnyLO0IgnFW5I5=s96-c" alt="" className="profilePic" />
-                <img src={DownArrowIcon} alt="" className="dropDown" />
+    const { user ,authenticatedUser } = useSelector((state) => state.user)
+
+    return (
+        <nav>
+            <div className="logo">LoGo</div>
+            <div className="searchbar">
+                <img src={SearchIcon} alt="" className='searchIcon' />
+                <input type="text" />
             </div>
-        </div>
-    </nav>
-  )
+            <div className="menus">
+                {
+                    authenticatedUser === false?
+                        <button className="loginbtn">Login</button> :
+                        <div className="loggedInMenus">
+                            <div className="profile"> 
+                                <h4 className="username">{user.name.split(' ')[0]}</h4>
+                                <img src={user?.profilePic} alt="" className="profilePic" />
+
+                            </div>
+                            <img src={DownArrowIcon} alt="" className="dropDown" />
+                        </div>
+
+                }
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
